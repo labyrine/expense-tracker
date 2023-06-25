2b53c71af43384e51fab37cf796141bdcb0042bd
Mette Munne <mette.ruusu@gmail.com>
2023-06-26 00:48:35 +0300

    Add edit feature for income/expense

--- history.py
+++ history.py
@@ -18,3 +18,21 @@ def delete_transaction(table_name, id_entry):
         db.session.execute(sql, {"id_entry": id_entry, "user_id": user_id})
     
     db.session.commit()
+
+def update_income(id_entry, amount, date, income_category, description):
+    sql = text("UPDATE income SET amount=:amount, date=:date, income_category=:income_category, description=:description WHERE id=:id_entry")
+    db.session.execute(sql, {"amount": amount, "date": date, "income_category": income_category, "description": description, "id_entry": id_entry})
+    db.session.commit()
+
+def update_expense(id_entry, amount, date, expense_category, description):
+    sql = text("UPDATE expenses SET amount=:amount, date=:date, expense_category=:expense_category, description=:description WHERE id=:id_entry")
+    db.session.execute(sql, {"amount": amount, "date": date, "expense_category": expense_category, "description": description, "id_entry": id_entry})
+    db.session.commit()
+
+def get_transaction_by_id(table_name, id_entry):
+    if table_name == 'income':
+        sql = text("SELECT 'Tulo' AS type, date, description, amount, income_category, id FROM income WHERE id = :id_entry")
+    elif table_name == 'expenses':
+        sql = text("SELECT 'Meno' AS type, date, description, amount, expense_category, id FROM expenses WHERE id = :id_entry")
+    result = db.session.execute(sql, {"id_entry": id_entry})
+    return result.fetchone()
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect, session, url_for
 import users, create_transactions, frontpage
-from history import get_transactions, delete_transaction
+from history import get_transactions, delete_transaction, update_income, update_expense, get_transaction_by_id
 
 @app.route("/")
 def index():
@@ -87,3 +87,57 @@ def delete_entry():
         return render_template("error.html", message="Luvaton toiminto.")
     delete_transaction(table_name, id_entry)
     return redirect("/history")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit_route():
+    if request.method == 'POST':
+        table_name = request.form.get('table_name')
+        id_entry = request.form.get('entry_id')
+        if session["csrf_token"] != request.form["csrf_token"]:
+            return render_template("error.html", message="Luvaton toiminto.")
+        if table_name == 'income':
+            return redirect(url_for("edit_income", id_entry=id_entry))
+        if table_name == 'expenses':
+            return redirect(url_for("edit_expense", id_entry=id_entry))
+        
+@app.route("/edit_income", methods=["GET", "POST"])
+def edit_income():
+    if request.method == 'POST':
+        id_entry = request.form.get('entry_id')
+        transaction = get_transaction_by_id('income', id_entry)
+        if session["csrf_token"] != request.form["csrf_token"]:
+           return render_template("error.html", message="Luvaton toiminto.")
+        description = request.form.get('description')
+        amount = request.form.get('amount')
+        date = request.form.get('date')
+        income_category = request.form.get('income_category')
+        update_income(id_entry, amount, date, income_category, description)
+        return redirect('/history')
+    else:
+        id_entry = request.args.get('id_entry')
+        transaction = get_transaction_by_id('income', id_entry)
+        if transaction is None:
+            return render_template("error.html", message="Tuloa ei löytynyt")
+        income_categories = create_transactions.get_income_categories()
+        return render_template('edit_transactions.html', form_type='income', income_categories=income_categories, transaction=transaction)
+
+@app.route("/edit_expense", methods=["GET", "POST"])
+def edit_expense():
+    if request.method == 'POST':
+        id_entry = request.form.get('entry_id')
+        transaction = get_transaction_by_id('expenses', id_entry)
+        if session["csrf_token"] != request.form["csrf_token"]:
+            return render_template("error.html", message="Luvaton toiminto.")
+        description = request.form.get('description')
+        amount = request.form.get('amount')
+        date = request.form.get('date')
+        expense_category = request.form.get('expense_category')
+        update_expense(id_entry, amount, date, expense_category, description)
+        return redirect('/history')
+    else:
+        id_entry = request.args.get('id_entry')
+        transaction = get_transaction_by_id('expenses', id_entry)
+        if transaction is None:
+            return render_template("error.html", message="Menoa ei löytynyt")
+        expense_categories = create_transactions.get_expense_categories()
+        return render_template('edit_transactions.html', form_type='expense', expense_categories=expense_categories, transaction=transaction)
